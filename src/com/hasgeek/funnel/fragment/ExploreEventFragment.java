a0d98732716babff9150af29dd2ce75f08a8b3de
Pratul Kalia <pratul@pratul.in>
2013-11-27 09:44:28 +0530

    ExploreEventFragment placeholder.

--- src/com/hasgeek/funnel/fragment/ExploreEventFragment.java
+++ src/com/hasgeek/funnel/fragment/ExploreEventFragment.java
@@ -1,10 +1,25 @@
 package com.hasgeek.funnel.fragment;
 
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 
 public class ExploreEventFragment extends Fragment {
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        TextView empty = new TextView(getActivity());
+        empty.setText("Coming soon, move along.");
+        empty.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        empty.setGravity(Gravity.CENTER);
 
+        return empty;
+    }
 }
