d78f1e90452d481b70764b90b4a3138f0ef234e7
Karthik Balakrishnan <karthikb351@gmail.com>
2014-12-04 15:04:18 +0530

    Fixed webview parameter on scrollback.
    Disabled remote debugging for playstore release
    
    Version Code 6
    Version Name 1.0.5

--- src/com/hasgeek/funnel/fragment/ExploreEventFragment.java
+++ src/com/hasgeek/funnel/fragment/ExploreEventFragment.java
@@ -1,14 +1,19 @@
 package com.hasgeek.funnel.fragment;
 
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 
 import com.hasgeek.funnel.R;
@@ -27,14 +32,39 @@ public class ExploreEventFragment extends Fragment {
         webView.setWebViewClient(mWebViewClient);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setDomStorageEnabled(true);
-
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            WebView.setWebContentsDebuggingEnabled(true);
-        }
-        webView.loadUrl("https://scrollback.io/hasgeek?"+URLEncoder.encode("webview={" +
-                "'nick' :'hasgeeknickname'" +
-                "}"));
+//        String nick;
+//        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getActivity());
+//
+//        if (!sp.getString("sb_nick", "1234").equalsIgnoreCase("1234")) {
+//            nick=sp.getString("sb_nick", "1234");
+//        }
+//        else
+//        {
+//            final EditText input = new EditText(getActivity());
+//            new AlertDialog.Builder(getActivity())
+//                    .setTitle("Update Status")
+//                    .setMessage("")
+//                    .setView(input)
+//                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+//                        final String in=input.getText().toString();
+//                        public void onClick(DialogInterface dialog, int whichButton) {
+//                            PreferenceManager.getDefaultSharedPreferences(getActivity())
+//                            .edit()
+//                            .putString("sb_nick",(in.equalsIgnoreCase("") ? "hasgeeknick" : in))
+//                            .apply();
+//                        }
+//                    }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+//                public void onClick(DialogInterface dialog, int whichButton) {
+//                    // Do nothing.
+//                }
+//            }).show();
+//            nick=sp.getString("sb_nick", "1234");
+//        }
+//
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+//            WebView.setWebContentsDebuggingEnabled(true);
+//        }
+        webView.loadUrl("https://scrollback.io/hasgeek?webview={}");
         mProgressBar = (ProgressBar) v.findViewById(R.id.pb_explore_event);
         return v;
     }
