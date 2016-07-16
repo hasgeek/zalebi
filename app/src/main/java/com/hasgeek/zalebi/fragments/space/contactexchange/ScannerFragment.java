f7f6629bcfb84ff5e2e2e9f1bbd26b7fcdea48c3
rajeef <rajeefmk@gmail.com>
2016-07-16 16:23:51 +0530

    Runtime Permission & Scannercode refactor
    Camera permission is requested when user click on Scan Badge.
    
    Scannercode is refactor as per new library update. mScannerView.startcamera
    in handleResult is replaced with mScannerView.resumeCameraPreview. This
    needs to delayed by 2 seconds for handling issues with older devices.

--- app/src/main/java/com/hasgeek/zalebi/fragments/space/contactexchange/ScannerFragment.java
+++ app/src/main/java/com/hasgeek/zalebi/fragments/space/contactexchange/ScannerFragment.java
@@ -3,19 +3,21 @@ package com.hasgeek.zalebi.fragments.space.contactexchange;
 /**
  * Created by karthikbalakrishnan on 25/03/15.
  */
+
+import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
-import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
+import com.hasgeek.zalebi.R;
 import com.hasgeek.zalebi.activity.SingleSpaceActivity;
 import com.hasgeek.zalebi.api.ContactExchangeService;
 import com.hasgeek.zalebi.api.model.Attendee;
@@ -30,6 +32,8 @@ import java.util.ArrayList;
 import me.dm7.barcodescanner.zbar.BarcodeFormat;
 import me.dm7.barcodescanner.zbar.Result;
 import me.dm7.barcodescanner.zbar.ZBarScannerView;
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
 
 public class ScannerFragment extends DialogFragment implements ZBarScannerView.ResultHandler {
     public ZBarScannerView mScannerView;
@@ -37,6 +41,7 @@ public class ScannerFragment extends DialogFragment implements ZBarScannerView.R
     private Bus mBus;
 
     private Space space;
+    private static final int REQUEST_CAMERA_PERMISSION = 101;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -46,7 +51,7 @@ public class ScannerFragment extends DialogFragment implements ZBarScannerView.R
         list.add(BarcodeFormat.CODE128);
         list.add(BarcodeFormat.QRCODE);
         mScannerView.setFormats(list);
-        mBus= BusProvider.getInstance();
+        mBus = BusProvider.getInstance();
         getDialog().setTitle("Scan Participant Badge");
         return mScannerView;
     }
@@ -55,8 +60,19 @@ public class ScannerFragment extends DialogFragment implements ZBarScannerView.R
     public void onResume() {
         super.onResume();
         mBus.register(this);
-        mScannerView.setResultHandler(this);
-        mScannerView.startCamera();
+        initScanner();
+    }
+
+    @AfterPermissionGranted(REQUEST_CAMERA_PERMISSION)
+    private void initScanner() {
+        String[] perms = {Manifest.permission.CAMERA};
+        if (EasyPermissions.hasPermissions(getContext(), perms)) {
+            mScannerView.setResultHandler(this);
+            mScannerView.startCamera();
+        } else {
+            EasyPermissions.requestPermissions(this, getString(R.string.rationale_message_for_camera_permission_in_scanner),
+                    REQUEST_CAMERA_PERMISSION, perms);
+        }
     }
 
     @Override
@@ -70,33 +86,45 @@ public class ScannerFragment extends DialogFragment implements ZBarScannerView.R
 
     @Override
     public void handleResult(Result rawResult) {
-        Log.i("handleResult()", "Raw Data:"+rawResult.getContents());
-        mScannerView.stopCamera();
+        Log.i("handleResult()", "Raw Data:" + rawResult.getContents());
         final Attendee a = ContactExchangeService.getAttendeeFromScannedData(rawResult.getContents(), space.getId(), space.getUrl(), getActivity());
-        if(a != null) {
+        if (a != null) {
             new AlertDialog.Builder(getActivity())
                     .setTitle("Add attendee?")
-                    .setMessage("Name: "+a.getFullname()+"\nCompany: "+a.getCompany())
+                    .setMessage("Name: " + a.getFullname() + "\nCompany: " + a.getCompany())
                     .setCancelable(false)
                     .setPositiveButton("Sync", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
-                            ContactExchangeService.addAttendeeToSyncQueue(a, space.getId(), space.getUrl() );
-                            mScannerView.startCamera();
+                            ContactExchangeService.addAttendeeToSyncQueue(a, space.getId(), space.getUrl());
+                            resumeScannerView();
 
                         }
                     })
                     .setNegativeButton("No", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
-                            mScannerView.startCamera();
+                            resumeScannerView();
                         }
                     })
                     .create().show();
+        } else {
+            resumeScannerView();
         }
-        else {
-            mScannerView.startCamera();
-        }
+    }
+
+    private void resumeScannerView() {
+        // Note from author:
+        // * Wait 2 seconds to resume the preview.
+        // * On older devices continuously stopping and resuming camera preview can result in freezing the app.
+        // * I don't know why this is the case but I don't have the time to figure out.
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mScannerView.resumeCameraPreview(ScannerFragment.this);
+            }
+        }, 2000);
     }
 
     @Override
@@ -104,5 +132,11 @@ public class ScannerFragment extends DialogFragment implements ZBarScannerView.R
         super.onPause();
         mBus.unregister(this);
         mScannerView.stopCamera();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
     }
 }
